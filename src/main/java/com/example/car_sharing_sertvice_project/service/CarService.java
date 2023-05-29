78e7ec09cff042d453d1cb5446805c0443b53c67
Ditkovskyi Pasha <pashkadit@gmail.com>
2023-05-29 20:14:38 +0300

    car service implemented

--- src/main/java/com/example/car_sharing_sertvice_project/service/CarService.java
+++ src/main/java/com/example/car_sharing_sertvice_project/service/CarService.java
@@ -1,11 +1,14 @@
 package com.example.car_sharing_sertvice_project.service;
 
+import com.example.car_sharing_sertvice_project.model.Car;
+import java.util.List;
+
 public interface CarService {
-    Object find(Long id);
+    Car find(Integer id);
 
-    void create(Object car);
+    Car save(Car car);
 
-    void update(Long id, Object car);
+    void delete(Integer id);
 
-    void delete(Long id);
+    List<Car> getAll();
 }
