0d6f66a09da7e07401e092edfd4c3db264ff4908
De1eF <jatfun58@gmail.com>
2023-05-29 11:08:51 +0200

    First project commit

---
+++ src/main/java/com/example/car_sharing_sertvice_project/CarSharingSertviceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_sharing_sertvice_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingSertviceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingSertviceProjectApplication.class, args);
+    }
+
+}
