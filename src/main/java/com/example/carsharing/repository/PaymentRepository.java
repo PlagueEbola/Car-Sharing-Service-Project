d893663f8f0689f2bc33a7b3c3f17ffeb0b8a4f8
Denys <denchik9026@gmail.com>
2023-06-02 00:27:50 +0300

    add authorization config and small fixes

--- src/main/java/com/example/carsharing/repository/PaymentRepository.java
+++ src/main/java/com/example/carsharing/repository/PaymentRepository.java
@@ -2,6 +2,8 @@ package com.example.carsharing.repository;
 
 import com.example.carsharing.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
