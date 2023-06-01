85b4f3d134e467cc7b7d32808e54fd81a1a16ca0
Denys <denchik9026@gmail.com>
2023-06-02 00:59:58 +0300

    checkstyle fix

--- src/main/java/com/example/carsharing/repository/UserRepository.java
+++ src/main/java/com/example/carsharing/repository/UserRepository.java
@@ -2,8 +2,8 @@ package com.example.carsharing.repository;
 
 import com.example.carsharing.model.User;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
