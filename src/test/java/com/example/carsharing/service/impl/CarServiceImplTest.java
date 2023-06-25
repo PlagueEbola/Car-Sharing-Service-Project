f2532242f8bd08f1e041279fe11ccfed228b26e5
Ditkovskyi Pasha <pashkadit@gmail.com>
2023-06-26 02:55:42 +0300

    implemented tests for CarServiceImpl

---
+++ src/test/java/com/example/carsharing/service/impl/CarServiceImplTest.java
@@ -0,0 +1,49 @@
+package com.example.carsharing.service.impl;
+
+import com.example.carsharing.model.Car;
+import com.example.carsharing.repository.CarRepository;
+import java.math.BigDecimal;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CarServiceImplTest {
+    @InjectMocks
+    private CarServiceImpl carService;
+
+    @Mock
+    private CarRepository carRepository;
+
+    @Test
+    public void updateByExistingCarId() {
+        Car carToSave = new Car("Camry", "Toyota", Car.CarType.SEDAN, 1, BigDecimal.valueOf(50));
+        Long carId = 14L;
+        Car savedCar = new Car(carId, "Camry", "Toyota", Car.CarType.SEDAN, 1, BigDecimal.valueOf(50));
+        Mockito.when(carRepository.existsById(14L)).thenReturn(true);
+        Mockito.when(carRepository.save(carToSave)).thenReturn(savedCar);
+
+        Car actual = carService.updateById(14L, carToSave);
+
+        Assertions.assertEquals(14L, actual.getId());
+        Assertions.assertEquals(carToSave.getModel(), actual.getModel());
+        Assertions.assertEquals(carToSave.getBrand(), actual.getBrand());
+        Assertions.assertEquals(carToSave.getType(), actual.getType());
+        Assertions.assertEquals(carToSave.getInventory(), actual.getInventory());
+        Assertions.assertEquals(carToSave.getDailyFee(), actual.getDailyFee());
+    }
+
+    @Test
+    public void testUpdateById_NonExistingCarId_ValidCar_ThrowsNoSuchElementException() {
+        Car carToSave = new Car("Camry", "Toyota", Car.CarType.SEDAN, 1, BigDecimal.valueOf(50));
+        Long carId = 1L;
+        Mockito.when(carRepository.existsById(carId)).thenReturn(false);
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> carService.updateById(carId, carToSave));
+    }
+}
