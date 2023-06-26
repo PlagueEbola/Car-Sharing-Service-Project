5d7410054d2f607e6a7dd652b12d26202c1616da
Ditkovskyi Pasha <pashkadit@gmail.com>
2023-06-26 10:16:17 +0300

    implemented CarControllerTest

--- src/main/java/com/example/carsharing/dto/response/CarResponseDto.java
+++ src/main/java/com/example/carsharing/dto/response/CarResponseDto.java
@@ -1,9 +1,13 @@
 package com.example.carsharing.dto.response;
 
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CarResponseDto {
     private Long id;
     private String model;
