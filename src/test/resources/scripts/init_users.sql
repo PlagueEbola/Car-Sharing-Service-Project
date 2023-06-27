facbab4d3f5087f587ef4a22c7fcb121e79b36c8
Pasha Ditkovskyi <58315455+Deschna@users.noreply.github.com>
2023-06-27 13:41:02 +0300

    refactored repository tests (#29)

--- src/test/resources/scripts/init_users.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (id, email, first_name, last_name, password) VALUES (1, 'alice@i.ua', 'Alice', 'Alison', 'alicePass');
-INSERT INTO users (id, email, first_name, last_name, password) VALUES (2, 'bob@i.ua', 'Bob', 'Bobson', 'bobPass');
